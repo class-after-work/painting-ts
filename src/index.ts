fb75eef2a46611342b3d81c1bc0e63019a0bba34
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2023-07-04 01:42:26 +0900

    add ~1-3 canvas api lecture

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+canvas.width = 800;
+canvas.height = 800;
+
+context.fillRect(0, 0, 100, 100);
+context.strokeRect(100, 100, 300, 300);
+
+setTimeout(() => {
+  context.beginPath();
+  context.rect(200, 200, 100, 100);
+  context.rect(300, 300, 100, 100);
+  context.rect(600, 600, 100, 100);
+  context.fillStyle = 'red';
+  context.fill();
+  context.closePath();
+}, 3000);
+
+context.beginPath();
+context.rect(700, 700, 100, 100);
+context.fillStyle = 'blue';
+context.fill();
+context.closePath();
